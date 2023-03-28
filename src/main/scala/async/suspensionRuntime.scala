08e9f82cbe3f00f743a12d263b0e83ecaa35ce59
odersky <odersky@gmail.com>
2023-03-28 18:02:41 +0200

    Drop unneeded sources
    Move sources that have to do with general suspension handling to
    a dotty test directory.

--- src/main/scala/async/suspensionRuntime.scala
+++ src/main/scala/async/suspensionRuntime.scala
@@ -1,7 +1,10 @@
 package runtime
 import scala.util.boundary, boundary.Label
 
-/** Contains a delimited contination, which can be invoked with `resume` */
+/** A hypothetical API for suspensions. Not yet implemented.
+ *  Suspension contain a delimited contination, which can be
+ *  invoked with `resume`
+ */
 class Suspension[-T, +R]:
   def resume(arg: T): R = ???
 
