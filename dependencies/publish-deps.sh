41872bd3650879b5366601171438bee2bb638a61
Natsu Kagami <nki@nkagami.me>
2023-12-01 18:27:17 +0100

    ci: Split jobs between native and jvm

--- dependencies/publish-deps.sh
+++ dependencies/publish-deps.sh
@@ -4,5 +4,10 @@ set -e
 
 cd "$(dirname "${BASH_SOURCE[0]}")" # change to this directory
 
-cd scala-native && sbt "publish-local-dev 3; ++3.1.2 publishLocal" && cd ..
-cd munit && sbt "++3.1.2 munitNative/publishLocal"
+cd scala-native && sbt 'publish-local-dev 3' && cd ..
+
+if test "$1" != "--scala-native-only"; then
+    cd scala-native && sbt '++3.1.2 publishLocal' && cd ..
+    cd munit && sbt "++3.1.2 munitNative/publishLocal" && cd ..
+fi
+
