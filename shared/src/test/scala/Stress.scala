8a3151d7195824b6206c78152996a043179a11c2
Natsu Kagami <nki@nkagami.me>
2023-12-18 19:29:41 +0100

    Add a stress test for creating lots of suspensions

--- shared/src/test/scala/Stress.scala
+++ shared/src/test/scala/Stress.scala
@@ -3,6 +3,8 @@ import gears.async.AsyncOperations.*
 import gears.async.default.given
 import gears.async.Future.MutableCollector
 import java.util.concurrent.atomic.AtomicInteger
+import gears.async.Timer
+import scala.concurrent.duration._
 
 class StressTest extends munit.FunSuite:
   test("survives a stress test that hammers on creating futures") {
@@ -19,5 +21,25 @@ class StressTest extends munit.FunSuite:
           collector += Future { compute }
         for i <- 1L to parallelism do sum += collector.results.read().right.get.await
         assertEquals(sum, total * (total + 1) / 2)
+  }
 
+  test("survives a stress test that hammers on suspending") {
+    val total = 100_000L
+    val parallelism = 5000L
+    Async.blocking:
+      val sleepy =
+        val timer = Timer(1.second)
+        Future { timer.run() }
+        timer.src
+      val k = AtomicInteger(0)
+      def compute(using Async) =
+        sleepy.awaitResult
+        k.incrementAndGet()
+      val collector = MutableCollector((1L to parallelism).map(_ => Future { compute })*)
+      var sum = 0L
+      for i <- parallelism + 1 to total do
+        sum += collector.results.read().right.get.await
+        collector += Future { compute }
+      for i <- 1L to parallelism do sum += collector.results.read().right.get.await
+      assertEquals(sum, total * (total + 1) / 2)
   }
