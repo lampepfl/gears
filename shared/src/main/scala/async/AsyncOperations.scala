9723588022967177747d3604746c86ab678cbb6b
Natsu Kagami <nki@nkagami.me>
2024-03-11 15:01:04 +0100

    Add a `sleep` method that takes scala.concurrent.duration, with documentation (#51)

--- shared/src/main/scala/async/AsyncOperations.scala
+++ shared/src/main/scala/async/AsyncOperations.scala
@@ -1,8 +1,22 @@
 package gears.async
 
+import scala.concurrent.duration.FiniteDuration
+
 trait AsyncOperations:
+  /** Suspends the current [[Async]] context for at least [[millis]] milliseconds. */
   def sleep(millis: Long)(using Async): Unit
 
 object AsyncOperations:
+  /** Suspends the current [[Async]] context for at least [[millis]] milliseconds.
+    * @param millis
+    *   The duration to suspend. Must be a positive integer.
+    */
   inline def sleep(millis: Long)(using AsyncOperations, Async): Unit =
     summon[AsyncOperations].sleep(millis)
+
+  /** Suspends the current [[Async]] context for at least [[millis]] milliseconds.
+    * @param duration
+    *   The duration to suspend. Must be positive.
+    */
+  inline def sleep(duration: FiniteDuration)(using AsyncOperations, Async): Unit =
+    sleep(duration.toMillis)
