062dab3d410f1e06afd5447a536808a00decfddd
Natsu Kagami <nki@nkagami.me>
2025-04-30 19:27:23 +0200

    Put a long timeout on Stress test

--- shared/src/test/scala/Stress.scala
+++ shared/src/test/scala/Stress.scala
@@ -8,13 +8,15 @@ import java.util.concurrent.atomic.AtomicInteger
 import scala.concurrent.duration._
 
 class StressTest extends munit.FunSuite:
+  override val munitTimeout: Duration = 10.minutes
+
   test("survives a stress test that hammers on creating futures") {
     val total = 200_000L
-    Seq[Long](1, 2, 4, 16, 10000).foreach: parallelism =>
-      val k = AtomicInteger(0)
-      def compute(using Async) =
-        k.incrementAndGet()
-      Async.blocking:
+    Async.blocking:
+      Seq[Long](1, 2, 4, 16, 10000).foreach: parallelism =>
+        val k = AtomicInteger(0)
+        def compute(using Async) =
+          k.incrementAndGet()
         val collector = MutableCollector((1L to parallelism).map(_ => Future { compute })*)
         var sum = 0L
         for i <- parallelism + 1 to total do
