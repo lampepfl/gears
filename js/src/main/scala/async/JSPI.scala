79a28e5d652ce154594b6086042a9c28b69715dd
Natsu Kagami <nki@nkagami.me>
2025-05-01 15:03:42 +0200

    Add documentation to WasmSuspend implementation

--- js/src/main/scala/async/JSPI.scala
+++ js/src/main/scala/async/JSPI.scala
@@ -3,6 +3,10 @@ package gears.async.js
 import scala.scalajs.js
 import scala.scalajs.js.annotation.*
 
+/** A stub implementation of the [[js.async]]/[[js.await]] functions, which are not yet available natively on Scala 3.
+  *
+  * See how the stubs are compiled in `build.sbt`.
+  */
 private[async] object JSPI:
   @inline
   def async[A](computation: => A): js.Promise[A] =
--- js/src/main/scala/async/WasmJSPISuspend.scala
+++ js/src/main/scala/async/WasmJSPISuspend.scala
@@ -6,6 +6,7 @@ import scala.compiletime.uninitialized
 import scala.scalajs.js
 import scala.scalajs.js.wasm.JSPI.allowOrphanJSAwait
 
+/** An opaque, compile-time token to signal that we are under a [[js.async]] scope. */
 opaque type AsyncToken = Unit
 
 object AsyncToken:
@@ -15,8 +16,18 @@ object AsyncToken:
 /** Capability-safe wrapper around [[JSPI.async]]. */
 private[async] inline def async[T](body: AsyncToken ?=> T): js.Promise[T] = JSPI.async(body(using ()))
 
-/** Note that this assumes that the root context is **already** under `JSPI.async`. */
+/** An implementation of [[SuspendSupport]] using JSPI async/await under WebAssembly.
+  * @note
+  *   this assumes that the root context is **already** under `JSPI.async`.
+  */
 trait WasmJSPISuspend(using AsyncToken) extends SuspendSupport:
+  /** The label stores a Promise that should be resolved every time the context is suspended or is completed. Since
+    * Promises are one-time resolvables, every resumption will "reset" the label, giving it a new Promise (see
+    * [[WasmLabel.reset]]).
+    *
+    * Due to the promise possibly changing over time, within [[boundary]], we have to dynamically resolve the reference
+    * _after_ running the `body`.
+    */
   protected class WasmLabel[T]():
     var (promise, resolve) = mkPromise[T]
 
@@ -25,19 +36,11 @@ trait WasmJSPISuspend(using AsyncToken) extends SuspendSupport:
       promise = p
       resolve = q
 
+  /** Creates a new [[js.Promise]] and returns both the Promise and its `resolve` function. */
   inline def mkPromise[T]: (js.Promise[T], T => Any) =
     var resolve: (T => Any) | Null = null
     val promise = js.Promise[T]((res, rej) => resolve = res)
     (promise, resolve)
-
-  opaque type Label[T] = WasmLabel[T]
-
-  override def boundary[T](body: Label[T] ?=> T): T =
-    val label = WasmLabel[T]()
-    JSPI.async:
-      val r = body(using label)
-      label.resolve(r)
-    JSPI.await(label.promise)
 
   protected class WasmSuspension[-T, +R](label: Label[R], resolve: T => Any) extends gears.async.Suspension[T, R]:
     def resume(arg: T): R =
@@ -45,19 +48,41 @@ trait WasmJSPISuspend(using AsyncToken) extends SuspendSupport:
       resolve(arg)
       JSPI.await(label.promise)
 
+  // Implementation of the [[SuspendSupport]] interface.
+
+  opaque type Label[T] = WasmLabel[T]
+
   opaque type Suspension[-T, +R] <: gears.async.Suspension[T, R] = WasmSuspension[T, R]
 
-  /** Should return immediately if resume is called from within body */
+  override def boundary[T](body: Label[T] ?=> T): T =
+    val label = WasmLabel[T]()
+    JSPI.async:
+      val r = body(using label)
+      label.resolve(r) // see [[WasmLabel]]
+    JSPI.await(label.promise) // this is fine to resolve immediately, since we only wait for the first return.
+
+  /** Suspends the context by creating a [[js.Promise]] to wait for inside the [[Suspension]] class, that would be
+    * resolved once resumed.
+    * @note
+    *   Should return immediately if resume is called from within body
+    */
   override def suspend[T, R](body: Suspension[T, R] => R)(using label: Label[R]): T =
     val (suspPromise, suspResolve) = mkPromise[T]
     val suspend = WasmSuspension[T, R](label, suspResolve)
     label.resolve(body(suspend))
     JSPI.await(suspPromise)
+end WasmJSPISuspend
 
+/** Overrides [[AsyncOperations]] with JavaScript-specific operations. */
 object JsAsyncOperations extends AsyncOperations:
   override def `yield`()(using Async) =
     sleep(1)
 
+/** An implementaion of [[Scheduler]] that assumes a single-threaded, event-loop driven JavaScript context.
+  *
+  * In this context, `execute` will always immediately run (while under a [[js.async]] scope, so that suspension is
+  * possible), while `schedule`d computations only run when another computation has yielded.
+  */
 object JsAsyncScheduler extends Scheduler:
   def execute(body: Runnable) = JSPI.async(body.run())
   def schedule(delay: scala.concurrent.duration.FiniteDuration, body: Runnable) =
@@ -66,22 +91,31 @@ object JsAsyncScheduler extends Scheduler:
       def cancel() =
         js.timers.clearTimeout(handle)
 
+/** An implementation of [[AsyncSupport]] where we assume a JSPI-enabled WebAssembly environment under a
+  * single-threaded, event-loop driven JavaScript scheduler (as assumed by [[JsAsyncScheduler]]).
+  */
 final class WasmAsyncSupport(using AsyncToken) extends AsyncSupport with WasmJSPISuspend:
   type Scheduler = JsAsyncScheduler.type
 
+/** A special root-level implementation of the [[Async]] context, that uses JSPI async/await on top-level to wait for
+  * futures.
+  */
 private[async] class JsAsync(val group: CompletionGroup)(using support: WasmAsyncSupport, sched: JsAsyncScheduler.type)
     extends Async(using support, sched):
   override def await[T](src: Async.Source[T]) =
     src
       .poll()
       .getOrElse:
-        JSPI.await:
+        JSPI.await: // TODO: can we make this more efficient?
           js.Promise: (resolve, _) =>
             src.onComplete:
               Listener: (item, _) =>
                 resolve(item)
   def withGroup(group: CompletionGroup) = JsAsync(group)
 
+/** An implementation of [[Async.FromSync]] that returns a [[scala.concurrent.Future]] for a top-level
+  * [[Async.blocking]] computation.
+  */
 object JsAsyncFromSync extends Async.FromSync:
   type Output[+T] = scala.concurrent.Future[T]
   def apply[T](body: Async ?=> T): Output[T] =
@@ -91,7 +125,7 @@ object JsAsyncFromSync extends Async.FromSync:
       Async.group(body)(using JsAsync(CompletionGroup.Unlinked))
     .toFuture
 
-/** Alternative [[Async.FromAsync]] implementation. Assumes** that we are under an `async` scope.
+/** Alternative [[Async.FromAsync]] implementation. **Assumes** that we are under an `async` scope.
   */
 object UnsafeJsAsyncFromSync extends Async.FromSync:
   type Output[+T] = T
